17ee099a474c624f098a592d4d78b478262b1fba
adil <adil_web99@outlook.com>
2020-07-23 13:11:07 -0400

    Added a test for the Popoover component

---
+++ src/__tests__/ComponentTests/PopOverTrigger.test.js
@@ -0,0 +1,25 @@
+/**
+ * @author Adil Merribi.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {fireEvent, render} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import PopOverTrigger from "../../components/PopOverButton";
+
+describe("PopOverTrigger test", () => {
+    describe("Component", () => {
+        it("no input arguments", () => {
+            const div = document.createElement('div');
+            ReactDOM.render(<PopOverTrigger/>, div);
+          });
+        it("testing text", () => {
+            const {getByText} = render(
+                <PopOverTrigger text='test text'>
+                  <div>test child</div>
+                </PopOverTrigger>)
+              expect(getByText('test text')).toBeInTheDocument();
+        });
+    });
+});
