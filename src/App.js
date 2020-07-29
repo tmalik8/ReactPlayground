c7452f6d67c40e08fc143caee4d8efb161f9d761
popsps <59066663+popsps@users.noreply.github.com>
2020-07-29 09:45:33 -0400

    v 0.01

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
+/**stylesheets */
 import './styles/LUX.css';
 import './styles/app.css';
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 /**React Components */
 import Container from "react-bootstrap/Container";
 
 
-// import "bootstrap/dist/css/bootstrap.min.css";
 /* Custom Components*/
 import Navigation from "./components/Navigation";
-import ShayanPage from "./components/interns/ShayanPage";
-import {BrowserRouter, Switch, Route} from "react-router-dom";
 import Home from "./components/Home";
 import PageNotFound from "./components/PageNotFound";
-
+import ShayanPage from "./components/interns/ShayanPage";
 
 const App = () => {
   return (
