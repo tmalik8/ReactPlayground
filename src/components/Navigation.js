c3353eb17acbc5b05093707a506b11dfdc44ffd2
iamamonte <iamamonte@gmail.com>
2020-07-30 20:01:25 -0400

    Update Navigation.js
    Minor change to set the source from a variable.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,9 +6,10 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Link from "react-router-dom/Link";
-
+import Image from 'react-bootstrap/Image';
 //stylesheet
 import '../styles/navigation.css';
+import logo from '../resources/turnUpLogo.png'
 
 const Navigation = () => {
   return (
@@ -16,7 +17,7 @@ const Navigation = () => {
       <Navbar.Toggle aria-controls="basic-navbar-nav"/>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="center-navbar m-auto">
-          <img alt="TurnUp Activism" src={require('../resources/turnUpLogo.png')} width="150" height="auto"/>
+        <Image className="align-top" alt="TurnUp Activism" src={logo} width="150" height="60"/>
           <Link className="nav-link" to='/'>Playground</Link>
           <Link className="nav-link" to='/events'>Events</Link>
           <Link className="nav-link" to='/conversations'>Conversations</Link>
