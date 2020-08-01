95118f0092f49b76e8a7325da734ab3ec6046124
Kyle Kobayashi <kobayashi14k@gmail.com>
2020-07-31 21:32:15 -0500

    created configureStore function in store/index

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from 'redux';
+import allReducers from '../reducers';
+
+const configureStore = () => {
+    const store = createStore(
+        allReducers,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+
+    return store;
+}
+
+export default configureStore;
