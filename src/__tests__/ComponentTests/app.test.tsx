99deb4f9fc721665e180678171d8d4201688afd3
Kyle Kobayashi <kobayashi14k@gmail.com>
2020-08-03 12:35:09 -0500

    removed unused import in app.test.tsx

--- src/__tests__/ComponentTests/app.test.tsx
+++ src/__tests__/ComponentTests/app.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../../App';
-import InputField from '../../components/InputField'
 
 describe("Initial Component Tests", () => {
     it('renders without crashing', () => {
