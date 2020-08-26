cf514517dd9d2716d7f3b674c63d1259e35a841e
iamamonte <iamamonte@gmail.com>
2020-08-25 20:42:14 -0400

    Changes to people component
    Passing in profile info as prop. Changing to Container instead of Form.

--- src/components/PeopleComponent.js
+++ src/components/PeopleComponent.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import Image from 'react-bootstrap/Image';
-import {Form} from "react-bootstrap";
+import {Container} from "react-bootstrap";
 import '../styles/PeopleComponent.css';
-import profilePic from '../resources/profilePicture.jpeg'
+
 
 const PeopleComponent = (props) => {
 
-    const person = {firstName: "Kyle", 
-                    lastName: "Kobayashi",
-                    city: "Miami",
-                    state: "FL",
-                    country: "US"}
+    const profile = props.profile;
 
     return (
-        <Form className="profile">
-            <Image className="profile-picture" src={profilePic}/>
-            <h1 className="profile-name">{person.firstName} {person.lastName}</h1>
-            <h2 className="profile-location">{person.city}, {person.state}, {person.country}</h2>
-        </Form>
+        <Container className="profile">
+            <Image className="profile-picture" src={profile.profileImage}/>
+            <h1 className="profile-name">{profile.firstName} {profile.lastName}</h1>
+            <h2 className="profile-location">{profile.city}, {profile.state}, {profile.country}</h2>
+        </Container>
     );
 };
 
