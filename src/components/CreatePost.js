cea6b3fe802daf03d1739fca2fab17cb7c7a064d
iamamonte <iamamonte@gmail.com>
2020-08-25 22:08:30 -0400

    Refactoring component styling

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -3,15 +3,24 @@
 //             img='https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg'/>
 
 import React from 'react';
+import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 import '../styles/CreatePost.css'
+
 
 const CreatePost = ({name, location, img}) => {
   return (
-      <div className='create-post-wrapper'>
-        <div className='cont'>
-          <div className='info-wrapper'>
-            <div className='info'>
+      <Container className='create-post-wrapper'>
+          <Container>
+           <Row>
+           <Col>
+           <div className='info'>
               <div className='img-container'>
+              {/** Will replace with People Component*/}
                 <img src={img} alt='me'/>
               </div>
               <div>
@@ -19,23 +28,29 @@ const CreatePost = ({name, location, img}) => {
                 <div className='loc'>{location}</div>
               </div>
             </div>
-            <div>
-              <div className='btn'>+ Create Events</div>
-            </div>
-          </div>
-          <div className='text-area'>
-            <textarea rows={4} placeholder="What's on your mind?"/>
-          </div>
-          <div className='flex-panel'>
-            <div className='tools'>
-              <i>picture</i>
-              <i>video</i>
-              <i>document</i>
-            </div>
-            <div className='btn'>OK</div>
-          </div>
-        </div>
-      </div>
+            </Col>
+            <Col lg={6} xs={6}>
+              <Button className="create-events-btn float-right">+ Create Events</Button>
+            </Col>
+           </Row>
+            
+            
+           
+          </Container>
+      <Form.Group>
+        <Form.Control as="textarea" placeholder="What's on your mind?" rows="3" />
+      </Form.Group>
+      <Form.Group>
+        <Row>
+          <Col><i>picture</i>
+            <i>video</i>
+            <i>document</i></Col>
+          <Col xs={6}> <Button className="create-post-btn float-right col-3">Ok</Button> </Col>
+        </Row>
+        
+      </Form.Group>
+      
+      </Container>
   );
 };
 
