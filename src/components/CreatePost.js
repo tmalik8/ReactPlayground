319720ebccc3a51a5d1db79aa08adbeb18c055cb
iamamonte <iamamonte@gmail.com>
2020-08-26 22:36:21 -0400

    Update CreatePost.js
    +Adding some placeholder holder state logic.
    +Allowing for a callback to be passed in via props.
    +Adding documentation

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,18 +1,49 @@
-// Example use case
-// <CreatePost name='James Foody' location='Cambridge, MA, US'
-//             img='https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg'/>
+/**
+ * @file CreatePost Component
+ * @author Sonia Saitawdekar 
+ */
 
-import React from 'react';
+
+
+
+import React, {useState} from 'react';
 import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
+
+
 import '../styles/CreatePost.css'
 
 
-const CreatePost = ({name, location, img}) => {
+const CreatePost = ({name, location, img, callback}) => {
+
+  //state
+  const [eventsToggled, toggleEventPost] = useState(true);
+  const [data, setData] = useState({});
+  
+  const _callback = callback;
+
+  /**
+   * Placeholder function.
+   * @param {function} callback - Executed after successful submission
+   */
+  function submitData(callback) {
+    console.log(`Create ${eventsToggled ? "event" : "post"}`, data);
+    if(_callback)  _callback(data);
+  }
+
+  /**
+   * Placeholder function.
+   * @param {string} text 
+   */
+  function updateAboutField(text) {
+    data.descrption = text;
+    setData(data)
+  }
+
   return (
       <Container className='create-post-wrapper'>
           <Container>
@@ -30,7 +61,7 @@ const CreatePost = ({name, location, img}) => {
             </div>
             </Col>
             <Col lg={6} xs={6}>
-              <Button className="create-events-btn float-right">+ Create Events</Button>
+              <Button className="create-events-btn float-right" onClick={() => toggleEventPost(!eventsToggled)}>+ Create {eventsToggled ? "Event" : "Post" } </Button>
             </Col>
            </Row>
             
@@ -38,14 +69,14 @@ const CreatePost = ({name, location, img}) => {
            
           </Container>
       <Form.Group>
-        <Form.Control as="textarea" placeholder="What's on your mind?" rows="3" />
+        <Form.Control as="textarea" placeholder="What's on your mind?" rows="3" onChange={(event)=> updateAboutField(event.target.value)} />
       </Form.Group>
       <Form.Group>
         <Row>
           <Col><i>picture</i>
             <i>video</i>
             <i>document</i></Col>
-          <Col xs={6}> <Button className="create-post-btn float-right col-3">Ok</Button> </Col>
+          <Col xs={6}> <Button className="create-post-btn float-right col-3" onClick={()=> submitData() } >Ok</Button> </Col>
         </Row>
         
       </Form.Group>
