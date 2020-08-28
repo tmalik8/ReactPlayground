6a4b7014d88f98edd41208bd2094204d8e7a720c
iamamonte <iamamonte@gmail.com>
2020-08-27 21:29:55 -0400

    Style refactoring

--- src/components/ConfirmationBox.js
+++ src/components/ConfirmationBox.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Row, Modal } from "react-bootstrap";
+import { Button, Row, Container, Modal } from "react-bootstrap";
 
 import '../styles/ConfirmationBox.css'
 
@@ -13,13 +13,16 @@ function ConfirmationBox(props) {
             <Modal.Header closeButton></Modal.Header>
 
             <Modal.Body>
-                <Row className='justify-content-center'><span className="body-modal-icon"><i class="fas fa-check-circle fa-3x"></i></span></Row>
-                <Row className='justify-content-center'><h1 className="body-modal-h1">Post Successfully!</h1></Row>
-                <Row className='justify-content-center'><p className="body-modal-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius tortor nibh</p></Row>
+                <Container className="confirm-box">
+                <Row> <span className="body-modal-icon"><i class="fas fa-check-circle fa-3x"></i></span></Row>
+                <Row ><h1>Post Successfully!</h1></Row>
+                <Row ><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius tortor nibh</p></Row>
+                <Row><Button onClick={props.onHide} className="button-modal">OK</Button></Row>
+                </Container>
             </Modal.Body>
 
-            <Row className='justify-content-center'><Button onClick={props.onHide} className="button-modal">OK</Button></Row>
-            <br />
+            
+            
         </Modal>
     );
 }
