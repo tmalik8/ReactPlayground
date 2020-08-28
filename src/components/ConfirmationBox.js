23952b035e8f8856f6fbc4659d732bf894424889
iamamonte <iamamonte@gmail.com>
2020-08-27 21:33:50 -0400

    Update ConfirmationBox.js
    Decoupling from modals
    
    Passing in content via props.

--- src/components/ConfirmationBox.js
+++ src/components/ConfirmationBox.js
@@ -1,29 +1,18 @@
 import React from "react";
-import { Button, Row, Container, Modal } from "react-bootstrap";
+import { Row, Container} from "react-bootstrap";
 
 import '../styles/ConfirmationBox.css'
 
-function ConfirmationBox(props) {
+function ConfirmationBox(header, content) {
+
+    const _header = header ?? "Success!";
+    const _content = content ?? "";
     return (
-        <Modal
-            {...props}
-            size="md"
-            backdrop="static"
-            centered>
-            <Modal.Header closeButton></Modal.Header>
-
-            <Modal.Body>
-                <Container className="confirm-box">
-                <Row> <span className="body-modal-icon"><i class="fas fa-check-circle fa-3x"></i></span></Row>
-                <Row ><h1>Post Successfully!</h1></Row>
-                <Row ><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas varius tortor nibh</p></Row>
-                <Row><Button onClick={props.onHide} className="button-modal">OK</Button></Row>
-                </Container>
-            </Modal.Body>
-
-            
-            
-        </Modal>
+        <Container className="confirm-box">
+            <Row> <span className="body-modal-icon"><i class="fas fa-check-circle fa-3x"></i></span></Row>
+            <Row ><h1>{_header}</h1></Row>
+            <Row ><p>{_content}</p></Row>
+        </Container>
     );
 }
 
