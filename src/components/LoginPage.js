53188beafeca5b06dd6dff408322f244fd07e821
sonias02 <sonias02@cs.washington.edu>
2020-08-31 14:39:08 -0700

    bootstrap for LoginPage

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import MissionsCarousel from "./MissionsCarousel";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import {Container, Row, Col, Form, Button} from "react-bootstrap";
 
 const items = [
   {
@@ -22,28 +21,30 @@ const items = [
 ]
 const LoginPage = () => {
   return (
-      <div className='login-page'>
-        <MissionsCarousel items={items}/>
-        <div className='welcome-section'>
+    <Container className='login-page'>
+      <Row>
+        <Col xs={12} md={6}>
+          <MissionsCarousel items={items}/>
+        </Col>
+        <Col xs={12} md={6} className='welcome-section'>
           <h2>Welcome to TurnUp</h2>
           <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
           <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
           <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
-          <div className='form-wrapper'>
-            <Form>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Control type="email" placeholder="E-mail"/>
-              </Form.Group>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Control type="password" placeholder="Password"/>
-              </Form.Group>
-              <Button variant="secondary" type="submit" block>
-                SIGN UP
-              </Button>
-            </Form>
-          </div>
-        </div>
-      </div>
+          <Form className='form-wrapper'>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Control type="email" placeholder="E-mail"/>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Control type="password" placeholder="Password"/>
+            </Form.Group>
+            <Button variant="secondary" type="submit" block>
+              SIGN UP
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
