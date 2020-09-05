8d5108534bcb07d760c55198951ba375974ce656
sonias02 <sonias02@cs.washington.edu>
2020-09-04 17:36:16 -0700

    pass in event and profile objects

--- src/components/DisplayEvent.js
+++ src/components/DisplayEvent.js
@@ -6,17 +6,7 @@ import {BsCalendar, BsClockFill, BsFillTagFill} from 'react-icons/bs';
 import '../styles/DisplayEvent.css';
 
 
-const DisplayEvent = (props) => {
-
-  const event = {
-    name: props.name || "Lorem ipsum dolor sit amet",
-    img: props.img || "https://cdn.pixabay.com/photo/2017/08/03/11/05/people-2575608_960_720.jpg",
-    description: props.description || "Ut tempus facilisis lacinia. Maecenas pharetra vel orci vitae tempor. Nulla sit amet ullamcorper ipsum. Vivamus vestibulum massa tortor, at luctus leo auctor ac. Praesent finibus dolor et luctus tincidunt. Phasellus ut neque eu nisl interdum luctus eu et nisi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi tempor sapien at faucibus mattis. Quisque venenatis tellus sed diam facilisis tempor. Vivamus ut mi at quam ultricies posuere. Maecenas in ipsum id quam maximus faucibus.",
-    tags: props.tags || ["Activism", "#TimeForChange"],
-    date: props.date || "Sunday, May 25, 2020",
-    time: props.time || "8:00 AM - 12:00 PM",
-    location: props.location || "Washington Square Park"
-  }
+const DisplayEvent = ({event, profile}) => {
 
   // format list of tags
   const renderedTags = [];
