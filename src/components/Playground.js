ef59c72a5fc5d1cff10d304653a19742535f6cc1
adil <adil_web99@outlook.com>
2020-09-10 20:41:58 -0400

    dummy form using formik and yup needs testing

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -31,6 +31,7 @@ import PeopleComponent from "./PeopleComponent";
 import CreatePost from "./CreatePost"
 import ConfirmationBox from "./ConfirmationBox";
 import MissionsCarousel from "./MissionsCarousel"
+import SignupForm from "./TestingForm"
 
 /* Models/Services */
 import {Intern} from "../model/Intern";
@@ -213,7 +214,7 @@ const Playground = (props) => {
         <Row className='justify-content-center'>
                 <ConfirmModal header={"Post Successful!"} content="Lorem ipsum ...." />
         </Row>
-
+        <SignupForm/>
         <Counter></Counter>
         <Container>
           <Row>
