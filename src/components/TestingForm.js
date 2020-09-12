2aba254efcf7db9f9aa1491f843b907f77074ec9
sonias02 <sonias02@cs.washington.edu>
2020-09-11 17:31:59 -0700

    condensed version of formik

--- src/components/TestingForm.js
+++ src/components/TestingForm.js
@@ -1,32 +1,32 @@
 
 import React from 'react';
-import { useFormik } from 'formik';
+import { useFormik, Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 const SignupForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-    },
-    validationSchema: Yup.object({
-      firstName: Yup.string()
-        .max(15, 'Must be 15 characters or less')
-        .required('Required'),
-      lastName: Yup.string()
-        .max(20, 'Must be 20 characters or less')
-        .required('Required'),
-      email: Yup.string()
-        .email('Invalid email address')
-        .required('Required'),
-    }),
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
+  // const formik = useFormik({
+  //   initialValues: {
+  //     firstName: '',
+  //     lastName: '',
+  //     email: '',
+  //   },
+  //   validationSchema: Yup.object({
+  //     firstName: Yup.string()
+  //       .max(15, 'Must be 15 characters or less')
+  //       .required('Required'),
+  //     lastName: Yup.string()
+  //       .max(20, 'Must be 20 characters or less')
+  //       .required('Required'),
+  //     email: Yup.string()
+  //       .email('Invalid email address')
+  //       .required('Required'),
+  //   }),
+  //   onSubmit: values => {
+  //     alert(JSON.stringify(values, null, 2));
+  //   },
+  // });
   return (
-    <form onSubmit={formik.handleSubmit}>
+    /*<form onSubmit={formik.handleSubmit}>
       <label htmlFor="firstName">First Name</label>
       <input
         id="firstName"
@@ -64,7 +64,43 @@ const SignupForm = () => {
         <div>{formik.errors.email}</div>
       ) : null}
       <button type="submit">Submit</button>
-    </form>
+    </form>*/
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+      }}
+      validationSchema={Yup.object({
+        firstName: Yup.string()
+          .max(15, 'Must be 15 characters or less')
+          .required('Required'),
+        lastName: Yup.string()
+          .max(20, 'Must be 20 characters or less')
+          .required('Required'),
+        email: Yup.string()
+          .email('Invalid email address')
+          .required('Required'),
+      })}
+      onSubmit={values => {
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      {() => (
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field name="firstName" />
+          <ErrorMessage name="firstName" />
+          <label htmlFor="lastName">Last Name</label>
+          <Field name="lastName" />
+          <ErrorMessage name="lastName" />
+          <label htmlFor="email">Email</label>
+          <Field name="email" type="email" />
+          <ErrorMessage name="email" />
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
