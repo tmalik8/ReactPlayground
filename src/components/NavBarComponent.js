0e14e7ba62fb0d1a2ebe15f424dbcc0d45a1ba22
iamamonte <iamamonte@gmail.com>
2020-09-14 19:52:37 -0400

    Improvements on navbar display

--- src/components/NavBarComponent.js
+++ src/components/NavBarComponent.js
@@ -3,43 +3,45 @@
  * @author - Adil Merribi
  */
 import React from "react";
-import { Navbar, Image, Nav } from "react-bootstrap";
+import { Navbar, Image, Nav, Form} from "react-bootstrap";
 import { AiFillHome, AiFillBell } from "react-icons/ai";
 import { BiCalendarEvent } from "react-icons/bi";
 import { FaUserFriends, FaBook, FaCog } from "react-icons/fa";
-import { IconContext } from "react-icons";
-
+import Link from "react-router-dom/Link"
 import logo from '../resources/logo.png'
 import '../styles/NavBarComponent.css';
 
 const NavBarComponent = ({ img }) => {
     return (
+        <>
         <Navbar collapseOnSelect className="turnup-nav" expand="lg">
             <Navbar.Brand href="#home"><Image className="logo" alt="TurnUp Activism" src={logo}/></Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="nav align-items-center links ">
-                    
-                    <IconContext.Provider value={{ size: 40, color: "white" }}>
+            <Form className="main-nav col-lg-9 col-sm-7 justify-content-center" inline>
+            
                     <Nav.Link href="#home"><AiFillHome /></Nav.Link>
                     <Nav.Link href="#events"><BiCalendarEvent /></Nav.Link>
                     <Nav.Link href="#friends"><FaUserFriends /></Nav.Link>
                     <Nav.Link href="#resources"><FaBook /></Nav.Link>
-                </IconContext.Provider>
-                    
-                
-                
-                <IconContext.Provider  value={{color: "white" }}>
-                    <Nav.Link href="#notifications"><AiFillBell /></Nav.Link>
-                    <Nav.Link href="#settings"><FaCog /></Nav.Link>
-                    <Nav.Link href="#profilePage"><Image className="profile-img" src={img} roundedCircle /></Nav.Link>
-                    <Nav.Link href="#SignOutPage"><div className="Logout-link">Log Out</div></Nav.Link>
-                </IconContext.Provider>
-                
-                </Nav>
-              
+            
+                </Form>
+                {/* <Nav className="justify-content-center mr-auto">
+
+                   
+
+
+                </Nav> */}
+                <Form className="side-nav  col-lg-3 col-sm-4" inline>
+                <Link href="notifications"><AiFillBell /></Link>
+                    <Link href="#settings"><FaCog /></Link>
+                    <Link href="#profilePage"><Image className="profile-img" src={img} roundedCircle /></Link>
+                    <Link href="#SignOutPage" className="logout">Log Out</Link>
+                </Form>
+               
             </Navbar.Collapse>
         </Navbar>
+        </>
     );
 };
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -13,11 +13,11 @@ import logo from '../resources/turnUpLogo.png'
 
 const Navigation = () => {
   return (
-    <Navbar variant="dark" expand="lg" className="navbar navbar-expand-lg">
+    <Navbar style={{backgroundColor:"cornflowerblue"}} variant="dark" expand="lg" className="navbar navbar-expand-lg">
       <Navbar.Toggle aria-controls="basic-navbar-nav"/>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="center-navbar m-auto">
-        
+        <Image className="align-top" alt="TurnUp Activism" src={logo} width="150" height="50"/>
           <Link className="nav-link" to='/'>Playground</Link>
           <Link className="nav-link" to='/events'>Events</Link>
           <Link className="nav-link" to='/conversations'>Conversations</Link>
