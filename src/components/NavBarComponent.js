d88f4f187e13593d583b63aae0eeec6e495c4273
iamamonte <iamamonte@gmail.com>
2020-09-14 20:50:00 -0400

    Navbar changes
    Wiring up links to work with react routing
    Adding playground links temporarily

--- src/components/NavBarComponent.js
+++ src/components/NavBarComponent.js
@@ -3,7 +3,7 @@
  * @author - Adil Merribi
  */
 import React from "react";
-import { Navbar, Image, Nav, Form} from "react-bootstrap";
+import { Navbar, Image, NavDropdown, Form} from "react-bootstrap";
 import { AiFillHome, AiFillBell } from "react-icons/ai";
 import { BiCalendarEvent } from "react-icons/bi";
 import { FaUserFriends, FaBook, FaCog } from "react-icons/fa";
@@ -13,25 +13,32 @@ import '../styles/NavBarComponent.css';
 
 const NavBarComponent = ({ img }) => {
     return (
-            <Navbar collapseOnSelect className="turnup-nav" expand="lg">
-                <Navbar.Brand href="#home"><Image className="logo" alt="TurnUp Activism" src={logo} /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Form className="main-nav col-lg-9 col-sm-8 justify-content-center" inline>
-                        <Nav.Link href="#home"><AiFillHome /><span>Home</span></Nav.Link>
-                        <Nav.Link href="#events"><BiCalendarEvent /><span>Events</span></Nav.Link>
-                        <Nav.Link href="#friends"><FaUserFriends /><span>Connect</span></Nav.Link>
-                        <Nav.Link href="#resources"><FaBook /><span>Resources</span></Nav.Link>
-                    </Form>
+        <Navbar collapseOnSelect className="turnup-nav" expand="lg">
+            <Navbar.Brand href="/TurnupHome"><Image className="logo" alt="TurnUp Activism" src={logo} /></Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Form className="main-nav col-lg-9 col-sm-8 justify-content-center" inline>
+                    <Link to="/TurnupHome"><AiFillHome /><span>Home</span></Link>
+                    <Link to="/Events"><BiCalendarEvent /><span>Events</span></Link>
+                    <Link to="/Connect"><FaUserFriends /><span>Connect</span></Link>
+                    <Link to="/Resources"><FaBook /><span>Resources</span></Link>
+                    <NavDropdown title="Interns" id="basic-nav-dropdown">
+                    <Link className="dropdown-item" to='/playground'>Playground</Link>
+                        <Link className="dropdown-item" to='/interns/reacttask2'>React Task 2</Link>
+                        <Link className="dropdown-item" to="/interns/soniaPage">Sonia's Page</Link>
+                        <Link className="dropdown-item" to="/interns/adilPage">Adil's Page</Link>
+                        <Link className="dropdown-item" to="/CreateAccount">Create Account</Link>
+                    </NavDropdown>
+                </Form>
 
-                    <Form className="side-nav  col-lg-3 col-sm-4" inline>
-                        <Link href="notifications"><AiFillBell /></Link>
-                        <Link href="#settings"><FaCog /></Link>
-                        <Link href="#profilePage"><Image className="profile-img" src={img} roundedCircle /></Link>
-                        <Link href="#SignOutPage" className="logout">Log Out</Link>
-                    </Form>
-                </Navbar.Collapse>
-            </Navbar>
+                <Form className="side-nav  col-lg-3 col-sm-4" inline>
+                    <Link to="/notifications"><AiFillBell /></Link>
+                    <Link to="/settings"><FaCog /></Link>
+                    <Link to="/profile"><Image className="profile-img" src={img} roundedCircle /></Link>
+                    <Link to="/logout" className="logout">Log Out</Link>
+                </Form>
+            </Navbar.Collapse>
+        </Navbar>
     );
 };
 
